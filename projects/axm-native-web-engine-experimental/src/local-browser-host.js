2e34bc34ea508dab28c7a6c412869629f9a07cba
mike-axiom-mir <miketobi90@gmail.com>
2026-08-23 15:38:25 +0200

    feat(web): bind AI research control to browser host

--- projects/axm-native-web-engine-experimental/src/local-browser-host.js
+++ projects/axm-native-web-engine-experimental/src/local-browser-host.js
@@ -3,12 +3,19 @@
 const Digest = require('./digest');
 const HostCore = require('./local-browser-host-core');
 const ShellPolicy = require('./shell-policy');
+const BrowserAiControl = require('./browser-ai-control');
 
 const HOST_RECEIPT_SCHEMA = 'axm.web.local-browser-host-receipt/v2';
 
 async function createLocalBrowserHost(session, options) {
+  options = options || {};
   const host = await HostCore.createLocalBrowserHost(session, options);
   const shellPolicy = ShellPolicy.buildShellPolicy();
+  const aiControl = options.aiControlPlane || new BrowserAiControl.LocalBrowserAiControl({
+    aiRegistry: options.aiRegistry,
+    researchRunner: options.researchRunner,
+    researchConfig: options.researchConfig
+  });
   const receiptMaterial = Object.assign({}, host.receipt, {
     schema: HOST_RECEIPT_SCHEMA,
     shellPolicySchema: shellPolicy.schema,
@@ -22,6 +29,9 @@ async function createLocalBrowserHost(session, options) {
     server: host.server,
     receipt,
     shellPolicy,
+    aiControl,
+    controlState: function () { return aiControl.state(); },
+    visualState: function () { return aiControl.visualState; },
     close: host.close
   };
 }
